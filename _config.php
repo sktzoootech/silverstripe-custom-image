8edd9a419293226b44ecb2f4df70283880c09cc5
ddimalen <davis@silverstripe.com>
2013-05-24 20:54:00 -0700

    Added CustomImageExtension.php and modified _config.php

--- _config.php
+++ _config.php
@@ -1,3 +1,10 @@
 <?php
 
+define('FALLBACKIMAGE_PATH', str_replace(str_replace('\\','/',
+                $_SERVER['DOCUMENT_ROOT']).'/', "",
+                str_replace('\\','/',dirname(__FILE__))) );
+
+Object::add_extension('Image', 'CustomImageExtension');
+
+CustomImageExtension::$fallback_image = FALLBACKIMAGE_PATH.'/image/no_image.png';
 	
---
+++ code/CustomImageExtension.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Decorates the Image class to be able to add additional features 
+ *
+ * @author Davis Dimalen <davis.dimalen@gmail.com>
+ * @package custom-image
+ */
+
+class CustomImageExtension extends DataExtension {
+
+	pubic static $fallback_image = null;
+
+	protected function failSafe(){	
+		if(!$this->isValid()){
+			
+			$this->owner->setFilename(self::$fallback_image);
+
+			$state = File::$update_filesystem;
+			File::$update_filesystem = false;
+			$this->owner->write();
+			File::$update_filesystem = $state;
+		}
+	}
+
+	public function isValid(){
+		return !$this->owner->Filename ||
+			!is_file($_SERVER['DOCUMENT_ROOT'].'/'.$this->owner->Filename) ||
+			is_dir($_SERVER['DOCUMENT_ROOT'].'/'.$this->owner->Filename) ? false : true ;
+	}
+
+	public function resizedAbsoluteURL($w, $h){
+		$this->failSafe();
+		return !$this->isValid()
+			? false
+			: Director::absoluteBaseURL().str_replace('%2F','/',
+				rawurlencode($this->owner->setSize($w, $h)->getFilename()));
+	}
+
+	public function croppedImageAbsoluteURL($w, $h){
+		$this->failSafe();
+		return !$this->isValid()
+			? false
+			: Director::absoluteBaseURL().str_replace('%2F','/',
+				rawurlencode($this->owner->CroppedImage($w, $h)->getFilename()));
+	}
+
+	public function setWidthAbsoluteURL($w, $option=1){
+		$this->failSafe();
+		return !$this->isValid()
+			? false
+			: Director::absoluteBaseURL().str_replace('%2F','/',
+				rawurlencode($this->owner->setWidth($w)->getFilename()));
+	}	
+
+	public function setSizeAbsoluteURL($w, $h) {
+		$this->failSafe();
+		return !$this->isValid()
+			? false
+			: Director::absoluteBaseURL().str_replace('%2F','/',
+				rawurlencode($this->owner->SetSize($w,$h)->getFilename()));
+	}
+}
